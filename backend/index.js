d4e6451812c20b0fbca541d79d5d4605c09a1603
angelaqu129 <angelaqu129@gmail.com>
2023-07-05 19:04:48 -0400

    upload files

---
+++ backend/index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3001;
+
+const boredape = require("./boredape.json");
+const doodles = require("./doodles.json");
+const azuki = require("./azuki.json");
+const meebits = require("./meebits.json");
+const moonbirds = require("./moonbirds.json");
+const coolcats = require("./coolcats.json");
+
+const path = require("path");
+
+const buildPath = path.join(__dirname, "../frontend/build");
+
+app.use(express.static(buildPath));
+app.use(cors());
+
+const files = [
+  {
+    contract: "0x8a90cab2b38dba80c64b7734e58ee1db38b8992e",
+    data: doodles,
+  },
+  {
+    contract: "0xED5AF388653567Af2F388E6224dC7C4b3241C544",
+    data: azuki,
+  },
+  {
+    contract: "0x7Bd29408f11D2bFC23c34f18275bBf23bB716Bc7",
+    data: meebits,
+  },
+  {
+    contract: "0x1A92f7381B9F03921564a437210bB9396471050C",
+    data: coolcats,
+  },
+  {
+    contract: "0x23581767a106ae21c074b2276D25e5C3e136a68b",
+    data: moonbirds,
+  },
+  {
+    contract: "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d",
+    data: boredape,
+  },
+];
+
+app.get("/nftCollection", (req, res) => {
+  const { query } = req;
+
+  const collection = files.find((e) => e.contract === query.contract);
+
+  if (!collection) {
+    return res.status(400).json({ error: "Invalid contract address" });
+  }
+
+  return res.status(200).json(collection.data);
+});
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(buildPath, "index.html"), (err) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening for API Calls`);
+});
---
+++ frontend/src/collections.js
@@ -0,0 +1,45 @@
+import moonbirds from "./images/moonbirds.png";
+import bayc from "./images/bayc.png";
+import azuki from "./images/azuki.png";
+import doodles from "./images/doodles.png";
+import meebits from "./images/meebits.png";
+import coolcats from "./images/coolcats.png";
+
+export const collections = [
+    {
+        "name": "Bored Ape Yacht Club",
+        "contract":"0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d",
+        "img": bayc,
+        "chain": "ETH"
+    },
+    {
+        "name": "Azuki",
+        "contract":"0xED5AF388653567Af2F388E6224dC7C4b3241C544",
+        "img": azuki,
+        "chain": "ETH"
+    },
+    {
+        "name": "Meebits",
+        "contract":"0x7Bd29408f11D2bFC23c34f18275bBf23bB716Bc7",
+        "img": meebits,
+        "chain": "ETH"
+    },
+    {
+        "name": "CryptoPunks",
+        "contract":"0x1A92f7381B9F03921564a437210bB9396471050C",
+        "img": coolcats,
+        "chain": "ETH"
+    },
+    {
+        "name": "Doodles",
+        "contract":"0x8a90cab2b38dba80c64b7734e58ee1db38b8992e",
+        "img": doodles,
+        "chain": "ETH"
+    },
+    {
+        "name": "Moonbirds",
+        "contract":"0x23581767a106ae21c074b2276D25e5C3e136a68b",
+        "img": moonbirds,
+        "chain": "ETH"
+    }
+]
